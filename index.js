1a968aeff80f02bc27bac969ca9c582a0803a83d
Maëlle <45653537+maellemoisis@users.noreply.github.com>
2023-02-21 09:16:13 +0000

    correcting uwu

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let startButton = document.querySelector('.starter')
-let startVideo = document.querySelector('#video')
-let startVideo2 = document.querySelector('#video2')
-let videoContainer = document.querySelector('#videoContainer')
+let startIntroVideo = document.querySelector('#intro-video')
+let startRoadVideo = document.querySelector('#road-video')
+let videoContainer = document.querySelector('#video-container')
 
 startButton.addEventListener('click', function(){
 startButton.classList.add('hidden')
