1b7730dff43041b32a2929229c1b43dfc589dca6
LeonieNollevalle <leonie.nollevalle@gmail.com>
2023-02-27 15:33:06 +0100

    finishh

--- wip/index.js
+++ wip/index.js
@@ -1,3 +1,22 @@
+let startButton = document.querySelector('.starter')
+let startIntroVideo = document.querySelector('#intro-video')
+let startRoadVideo = document.querySelector('#road-video')
+let videoContainer = document.querySelector('#video-container')
+let containerIntro = document.querySelector('.intro');
+
+startButton.addEventListener('click', function(){
+startButton.classList.add('hidden')
+startIntroVideo.play();
+setTimeout(() => {
+  videoContainer.classList.remove('hidden');
+  videoContainer.classList.add('road'); 
+  startRoadVideo.play(); 
+  containerIntro.classList.remove('intro');
+  containerIntro.classList.remove('hidden')
+  startIntroVideo.classList.add('hidden');
+}, "15000")
+});
+
 window.onload =()=>{
 
 // gear-lever step
@@ -46,9 +65,26 @@ let factory = new Audio('../assets/sounds/factory-.mp3');
 let metal = new Audio('../assets/sounds/metal-impact.mp3');
 let klaxon = new Audio('../assets/sounds/klaxon.mp3');
 let frein = new Audio('../assets/sounds/car-skid.mp3');
+let futur = new Audio('../assets/sounds/Futuristic-interface.mp3');
+
 
 // css file
-let cssLink = document.querySelector('#css');
+var cssLink = document.getElementById('css');
+
+
+function goToStep2 () {
+  let transformed = document.getElementsByClassName("transformed");
+  if (transformed.length == 9) {
+    cssLink.setAttribute('href', 'step2.css');
+    foot1Img.src = "../assets/images/edited-body/foot1.png";
+    arm1Img.src = "../assets/images/edited-body/arm1.png";
+
+  }
+}
+
+// video speed 
+
+startRoadVideo.playbackRate = 1;
 
 
 speedOne.addEventListener('click', function(){
@@ -57,6 +93,7 @@ speedOne.addEventListener('click', function(){
   knob.style.left = "5%";
   head.classList.remove('hidden');
   head.classList.add('head');
+  startRoadVideo.playbackRate  = startRoadVideo.playbackRate + 0.5;
 });
 
 speedTwo.addEventListener('click', function(){
@@ -67,6 +104,7 @@ speedTwo.addEventListener('click', function(){
     knob.style.left = "5%";
     trunk.classList.remove('hidden');
     trunk.classList.add('trunk');
+    startRoadVideo.playbackRate  = startRoadVideo.playbackRate + 0.5;
   } else {
     alert('Passe la première !')
   }
@@ -83,6 +121,7 @@ speedThree.addEventListener('click', function(){
   arm1.classList.add('arm1');
   arm2.classList.remove('hidden');
   arm2.classList.add('arm2');
+  startRoadVideo.playbackRate  = startRoadVideo.playbackRate + 0.5;
   } else {
     alert('Passe la seconde !')
   }
@@ -101,6 +140,7 @@ speedFour.addEventListener('click', function(){
     foot2.classList.add('foot2');
     numberplate.classList.remove('hidden');
     numberplate.classList.add('numberplate');
+    startRoadVideo.playbackRate  = startRoadVideo.playbackRate + 0.5;
   }
   else {
     alert('Passe la troisième !')
@@ -120,6 +160,7 @@ speedFive.addEventListener('click', function(){
     leg2.classList.add('leg2');
     sticker.classList.remove('hidden');
     sticker.classList.add('sticker');
+    startRoadVideo.playbackRate  = startRoadVideo.playbackRate + 0.5;
   } else {
     alert('Passe la quatrième !')
   }
@@ -138,7 +179,8 @@ headImg.addEventListener('click', function(){
   blinker.play();
   blinker.currentTime = 0;
   head.classList.add('transformed');
-
+  startRoadVideo.playbackRate  = startRoadVideo.playbackRate + 0.5;
+  goToStep2();
 
 })
 
@@ -152,6 +194,10 @@ trunkImg.addEventListener('click', function(){
   engine.volume=0.15;
   engine.play();
   engine.currentTime = 0;
+  trunk.classList.add('transformed');
+  startRoadVideo.playbackRate  = startRoadVideo.playbackRate + 0.5;
+  goToStep2();
+  
 })
 
 arm1Img.addEventListener('click', function(){
@@ -163,6 +209,10 @@ arm1Img.addEventListener('click', function(){
   metal.volume=0.15;
   metal.play();
   metal.currentTime = 0;
+  arm1.classList.add('transformed');
+  startRoadVideo.playbackRate  = startRoadVideo.playbackRate + 0.5;
+  goToStep2();
+
 })
 
 arm2Img.addEventListener('click', function(){
@@ -174,6 +224,10 @@ arm2Img.addEventListener('click', function(){
   metal.volume=0.15;
   metal.play();
   metal.currentTime = 0;
+  arm2.classList.add('transformed');
+  startRoadVideo.playbackRate  = startRoadVideo.playbackRate + 0.5;
+  goToStep2();
+
 })
 
 leg1Img.addEventListener('click', function(){
@@ -186,6 +240,12 @@ leg1Img.addEventListener('click', function(){
   factory.volume=0.5;
   factory.play();
   factory.currentTime = 0;
+  leg1.classList.add('transformed');
+  startRoadVideo.playbackRate  = startRoadVideo.playbackRate + 0.5;
+
+
+  goToStep2();
+
 })
 
 leg2Img.addEventListener('click', function(){
@@ -198,6 +258,10 @@ leg2Img.addEventListener('click', function(){
   factory.volume=0.5;
   factory.play();
   factory.currentTime = 0;
+  leg2.classList.add('transformed');
+  startRoadVideo.playbackRate  = startRoadVideo.playbackRate + 0.5;
+  goToStep2();
+
 })
 
 foot1Img.addEventListener('click', function(){
@@ -210,6 +274,10 @@ foot1Img.addEventListener('click', function(){
   frein.volume=0.5;
   frein.play();
   frein.currentTime = 0;
+  foot1.classList.add('transformed');
+  startRoadVideo.playbackRate  = startRoadVideo.playbackRate + 0.5;
+  goToStep2();
+
 })
 
 foot2Img.addEventListener('click', function(){
@@ -222,16 +290,45 @@ foot2Img.addEventListener('click', function(){
   frein.volume=0.5;
   frein.play();
   frein.currentTime = 0;
+  foot2.classList.add('transformed');
+  startRoadVideo.playbackRate  = startRoadVideo.playbackRate + 0.5;
+  goToStep2();
+
 })
 
 numberplate.addEventListener('click', function(){
   klaxon.play();
+  numberplate.classList.add('transformed');
+  futur.play();
+  goToStep2();
+  startRoadVideo.playbackRate  = startRoadVideo.playbackRate + 0.5;
 })
 
+crash.addEventListener('click', function(){
+  let transformed = document.getElementsByClassName("transformed");
+  if (transformed.length == 9) {
+  let carCrash = new Audio('../assets/sounds/car-crash.mp3');
+  let carSkid = new Audio('../assets/sounds/car-skid.mp3');
+  klaxon.pause();
+  frein.pause();
+  engine.pause();
+  blinker.pause();
+  factory.pause();
+  startRoadVideo.pause();
+  futur.pause();
+  carSkid.play();
+  carCrash.play();
+  setTimeout(() => {
+    window.location.href = "step3.html"
+  }, 1200);
+} else {
+  alert('Transformer le corps en cliquant sur chacune des parties !')
+}
+});
 
 
-if (1 > 0) {
-  console.log("nullos")
 }
 
-}
+console.log(speed);
+
+
---
+++ wip/step3.js
@@ -0,0 +1,7 @@
+window.onload =()=>{
+  let carCrash = new Audio('../assets/sounds/car-crash.mp3');
+  let carSkid = new Audio('../assets/sounds/car-skid.mp3');
+  carSkid.play();
+  carCrash.play();
+  };
+
